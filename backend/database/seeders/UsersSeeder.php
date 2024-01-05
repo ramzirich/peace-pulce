0397658e4629fbaef28fbda22f55f34a34e0c776
ramziriche <ramziriche96@gmail.com>
2024-01-05 23:17:49 +0200

    backend:seed users data

--- backend/database/seeders/UsersSeeder.php
+++ backend/database/seeders/UsersSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class UsersSeeder extends Seeder
 {
@@ -13,5 +14,121 @@ class UsersSeeder extends Seeder
     public function run(): void
     {
         //
+        DB::table('users')->insert([
+            'first_name' => 'George',
+            'last_name'=> 'Atiyeh',
+            'email' =>'georges@gmail.com',
+            'password' =>'R@mzi123',
+            'img_url' => 'images/user.jpg',
+            'role_id' =>1
+        ]);
+
+        DB::table('users')->insert([
+            'first_name' => 'Ali',
+            'last_name'=> 'Kassem',
+            'email' =>'alikassem@gmail.com',
+            'password' =>'R@mzi123',
+            'img_url' => 'images/user2.jpg',
+            'role_id' =>1
+        ]);
+
+        DB::table('users')->insert([
+            'first_name' => 'Omar',
+            'last_name'=> 'Daouk',
+            'email' =>'omarDaouk@gmail.com',
+            'password' =>'R@mzi123',
+            'img_url' => 'images/user5.jpg',
+            'role_id' =>1
+        ]);
+
+        DB::table('users')->insert([
+            'first_name' => 'Nadim',
+            'last_name'=> 'Nadam',
+            'email' =>'nadimNadam@gmail.com',
+            'password' =>'R@mzi123',
+            'img_url' => 'images/user10.jpg',
+            'role_id' =>1
+        ]);
+
+        DB::table('users')->insert([
+            'first_name' => 'Brad',
+            'last_name'=> 'Notpitt',
+            'email' =>'bradNotpitt@gmail.com',
+            'password' =>'R@mzi123',
+            'img_url' => 'images/psy1.jpg',
+            'role_id' =>2
+        ]);
+
+        DB::table('users')->insert([
+            'first_name' => 'Ashin',
+            'last_name'=> 'Venkat',
+            'email' =>'ashinVenkat@gmail.com',
+            'password' =>'R@mzi123',
+            'img_url' => 'images/psy2.jpg',
+            'role_id' =>2
+        ]);
+
+        DB::table('users')->insert([
+            'first_name' => 'Elsa',
+            'last_name'=> 'Frozen',
+            'email' =>'elsaFrozen@gmail.com',
+            'password' =>'R@mzi123',
+            'img_url' => 'images/psy3.jpg',
+            'role_id' =>2
+        ]);
+
+        DB::table('users')->insert([
+            'first_name' => 'Simon',
+            'last_name'=> 'Casper',
+            'email' =>'simonCasper@gmail.com',
+            'password' =>'R@mzi123',
+            'img_url' => 'images/psy4.jpg',
+            'role_id' =>2
+        ]);
+
+        DB::table('users')->insert([
+            'first_name' => 'Serena',
+            'last_name'=> 'Selena',
+            'email' =>'serenaSelena@gmail.com',
+            'password' =>'R@mzi123',
+            'img_url' => 'images/psy5.jpg',
+            'role_id' =>2
+        ]);
+
+        DB::table('users')->insert([
+            'first_name' => 'george',
+            'last_name'=> 'atiyeh',
+            'email' =>'georges@gmail.com',
+            'password' =>'R@mzi123',
+            'img_url' => 'images/user.jpg',
+            'role_id' =>1
+        ]);
+
+        DB::table('users')->insert([
+            'first_name' => 'Patrick',
+            'last_name'=> 'Cruise',
+            'email' =>'patrickCruise@gmail.com',
+            'password' =>'R@mzi123',
+            'img_url' => 'images/psy7.jpg',
+            'role_id' =>2
+        ]);
+
+        DB::table('users')->insert([
+            'first_name' => 'Pragim',
+            'last_name'=> 'Paghich',
+            'email' =>'pragimPaghich@gmail.com',
+            'password' =>'R@mzi123',
+            'img_url' => 'images/psy7.png',
+            'role_id' =>2
+        ]);
+
+        DB::table('users')->insert([
+            'first_name' => 'Jean',
+            'last_name'=> 'Dreamer',
+            'email' =>'jeanDremaer@gmail.com',
+            'password' =>'R@mzi123',
+            'img_url' => 'images/pexels-photo-663455.jpeg',
+            'role_id' =>3
+        ]);
     }
 }
