b6b149641146026307425bbd8294fb4bce7d5335
ramziriche <ramziriche96@gmail.com>
2024-01-05 23:51:39 +0200

    backend:seed patient_doctor_request data

--- backend/database/seeders/PatientRequestsSeeder.php
+++ backend/database/seeders/PatientRequestsSeeder.php
@@ -16,31 +16,49 @@ class PatientRequestsSeeder extends Seeder
         //
         DB::table('patient_doctor_requests')->insert([
             'patient_id' => 1,
-            'doctor_id'=> 5,
+            'doctor_id'=> 1,
             'request' =>'accepted'
         ]);
 
         DB::table('patient_doctor_requests')->insert([
             'patient_id' => 1,
+            'doctor_id'=> 3,
+            'request' =>'accepted'
+        ]);
+
+        DB::table('patient_doctor_requests')->insert([
+            'patient_id' => 2,
             'doctor_id'=> 5,
             'request' =>'accepted'
         ]);
 
         DB::table('patient_doctor_requests')->insert([
-            'patient_id' => 1,
-            'doctor_id'=> 5,
+            'patient_id' => 2,
+            'doctor_id'=> 7,
             'request' =>'accepted'
         ]);
 
         DB::table('patient_doctor_requests')->insert([
-            'patient_id' => 1,
-            'doctor_id'=> 5,
+            'patient_id' => 3,
+            'doctor_id'=> 3,
             'request' =>'accepted'
         ]);
 
         DB::table('patient_doctor_requests')->insert([
-            'patient_id' => 1,
-            'doctor_id'=> 5,
+            'patient_id' => 3,
+            'doctor_id'=> 4,
+            'request' =>'accepted'
+        ]);
+
+        DB::table('patient_doctor_requests')->insert([
+            'patient_id' => 4,
+            'doctor_id'=> 7,
+            'request' =>'accepted'
+        ]);
+
+        DB::table('patient_doctor_requests')->insert([
+            'patient_id' => 4,
+            'doctor_id'=> 6,
             'request' =>'accepted'
         ]);
     }
