7463664c39923e0a5fee65121f04f5da9ee3be1b
ramziriche <ramziriche96@gmail.com>
2024-01-11 11:19:37 +0200

    backend: add song validation

---
+++ backend/app/Http/Requests/Song/SongRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests\Song;
+use Illuminate\Validation\ValidationException;
+
+class SongRequest{
+    public static function createSongValidation($request){
+        try{
+            $request->validate([
+                "title" => "required",
+                "filename" => "required|mimes:mp3,wav",
+                "artist" => "required",
+            ],[
+                "title.required" => "The title field is required",
+                "filename.required" => "The filename field is required",
+                "filename.mimes:mp3,wav" => "The filename field must contain mp3 or wav",
+                "artist.required" => "The arttist field is required",
+            ]);
+            return response()->json([
+                'status' => "success"
+            ]);
+        }catch (ValidationException $exception) {
+            return response()->json([
+                'status' => "error",
+                'errors' => $exception->errors()
+            ], 422);
+        }
+    }
+}
