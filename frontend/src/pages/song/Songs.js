bdf801b21a2a65cb65796d1767010912a15e3ee2
ramziriche <ramziriche96@gmail.com>
2024-01-11 13:21:33 +0200

    frontend: add song page color and left icon

--- frontend/src/pages/song/Songs.js
+++ frontend/src/pages/song/Songs.js
@@ -1,10 +1,29 @@
 import React from "react"
-import { View } from "react-native"
+import { Image, StyleSheet, View } from "react-native"
+import LinearGradient from "react-native-linear-gradient"
+import { CustomColors } from "../../styles/color"
 
 export default Songs = () =>{
     return(
-        <View style={{flex:1, backgroundColor:'red'}}>
-
-        </View>
+        <LinearGradient colors={['#a34c0d', '#592804', '#241001', '#000000']}
+            style={styles.container}
+        >
+            <Image source={require('../../../assets/songImages/left.png')}
+                style={styles.icons}
+            /> 
+        </LinearGradient>
     )
 } 
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        paddingTop:30,
+        paddingHorizontal:20,
+    },
+    icons:{
+        width:20,
+        height:20,
+        tintColor:CustomColors.white
+    }
+})
