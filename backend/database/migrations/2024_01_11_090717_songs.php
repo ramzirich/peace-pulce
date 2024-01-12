c486cc2d69adee6dab058f2cf9047782c37fa187
ramziriche <ramziriche96@gmail.com>
2024-01-12 21:24:38 +0200

    backend: add artwork in songs migration

--- backend/database/migrations/2024_01_11_090717_songs.php
+++ backend/database/migrations/2024_01_11_090717_songs.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('title');
             $table->string('url');
             $table->string('artist');
+            $table->string('artwork');
             $table->timestamps();
         });
     }
