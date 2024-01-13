6b5630676092b7503a27f483fed2dc47d202201d
ramziriche <ramziriche96@gmail.com>
2024-01-13 15:48:07 +0200

    frontend:add img comment

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -27,7 +27,7 @@ const Home = ({navigation}) => {
   return ( 
      <ScrollView> 
        <CustomHeader/>
-       <Image source={require('../../../assets/images/1.jpg')} style={{width:200, height:340, alignSelf:'center'}}/>
+       {/* <Image source={require('../../../assets/images/1.jpg')} style={{width:200, height:340, alignSelf:'center'}}/> */}
        <HeaderButton navigation={navigation} />
        <SliderHorizental videos={videos} />
     </ScrollView>      
