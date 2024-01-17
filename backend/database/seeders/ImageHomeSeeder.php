1fe9712a650ccfe94f4b262be7f42513750579b4
ramziriche <ramziriche96@gmail.com>
2024-01-17 12:53:23 +0200

    backend:add data to imageHome seeder

--- backend/database/seeders/ImageHomeSeeder.php
+++ backend/database/seeders/ImageHomeSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ImageHomeSeeder extends Seeder
 {
@@ -13,5 +14,60 @@ class ImageHomeSeeder extends Seeder
     public function run(): void
     {
         //
+        $quotes = [
+            [
+                'text' => "The only limit to our realization of tomorrow will be our doubts of today.",
+                'author' => 'Franklin D. Roosevelt',
+                'img_url' => '/images/home/8.jpg',
+            ],
+            [
+                'text' => "Believe you can and you\'re halfway there.",
+                'author' => 'Theodore Roosevelt',
+                'img_url' => '/images/home/9.jpg',
+            ],
+            [
+                'text' => "Your time is limited, don't waste it living someone else's life.",
+                'author' => 'Steve Jobs',
+                'img_url' => '/images/home/12.jpg',
+            ],
+            [
+                'text' => "Your life does not get better by chance, it gets better by change.",
+                'author' => 'Jim Rohn',
+                'img_url' => '/images/home/13.jpg',
+            ],
+            [
+                'text' => 'The only way to do great work is to love what you do.',
+                'author' => 'Steve Jobs',
+                'img_url' => '/images/home/14.jpg',
+            ],
+            [
+                'text' => "What lies behind us and what lies before us are tiny matters compared to what lies within us.",
+                'author' => 'Ralph Waldo Emerson',
+                'img_url' => '/images/home/15.jpg',
+            ],
+            [
+                'text' => "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+                'author' => 'Winston Churchill',
+                'img_url' => '/images/home/16.jpg',
+            ],
+            [
+                'text' => 'How much further could we march, if we were not forced to carry our fears on our backs?',
+                'author' => 'Pantheon',
+                'img_url' => '/images/home/18.jpg',
+            ],
+            [
+                'text' => "It is never too late to be what you might have been.",
+                'author' => 'George Eliot',
+                'img_url' => '/images/home/19.jpg',
+            ],
+        ];
+
+        foreach ($quotes as $quote) {
+            DB::table('home-images')->insert([
+                'text' => $quote['text'],
+                'author' => $quote['author'],
+                'img_url' => $quote['img_url'],
+            ]);
+        }
     }
 }
