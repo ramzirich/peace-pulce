fd538daf3300a36156b9e73779e13d8cdc1bc130
ramziriche <ramziriche96@gmail.com>
2024-01-18 01:43:06 +0200

    frontend: clean code

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,13 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import { Image, ScrollView, View } from 'react-native';
+import React from 'react';
+import { View } from 'react-native';
 import { CustomHeader } from '../../reusable/components/header/CustomHeader';
 import { HeaderButton } from '../../reusable/components/headerButtons/HeaderButtons';
-import { config } from '../../../config';
-import axios from 'axios';
-import { SliderHorizental } from '../../reusable/components/sliderHorizental/SliderHorizental';
-import { useSelector } from 'react-redux';
-import LinearGradient from 'react-native-linear-gradient';
-import HomeSwiper from '../../reusable/components/swiper/HomeSwiper';
 import SwiperComponent from '../../reusable/components/swiper/SwiperComponenet';
 import FooterButtons from '../../reusable/components/footerButtons/footerButtons';
 
