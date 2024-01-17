c0d65b81f80f7e63a27e1aec3b40081acc3094c9
ramziriche <ramziriche96@gmail.com>
2024-01-17 15:45:44 +0200

    backend:add homeimages model

---
+++ backend/app/Models/HomeImages.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class HomeImages extends Model
+{
+    use HasFactory;
+}
