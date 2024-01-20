a49abc1a20e2657cbebbe28545052973d8cb740b
ramziriche <ramziriche96@gmail.com>
2024-01-21 01:29:14 +0200

    backend: add update validation to problem

--- backend/app/Http/Requests/Problem/ProblemRequest.php
+++ backend/app/Http/Requests/Problem/ProblemRequest.php
@@ -12,7 +12,7 @@ class ProblemRequest{
                 "severity" => "required|numeric|between:0,10",
             ],[
                 "problem.required" => "The Problem field is required",
-                "problem.string" => "The Problem field must be a stringaction",
+                "problem.string" => "The Problem field must be a string",
 
                 "action.required" => "The action field is required",
                 "action.string" => "The action field must be a string",
@@ -20,6 +20,40 @@ class ProblemRequest{
                 "severity.required" => "The severity field is required",
                 "severity.numeric" => "The severity field must be numeric",
                 "severity.between" => "The severity field must be from 0 to 10"
+            ]);
+            return response()->json([
+                'status' => "success"
+            ]);
+        }catch (ValidationException $exception) {
+            return response()->json([
+                'status' => "error",
+                'errors' => $exception->errors()
+            ], 422);
+        }
+    }
+
+    public static function updateProblemValidation($request){
+        try{
+            $request->validate([
+                "problem" => "required|string",
+                "action" => "required|string",
+                "severity" => "required|numeric|between:0,10",
+                "solution" => "string",
+                "ai_solution" =>"string"
+            ],[
+                "problem.required" => "The Problem field is required",
+                "problem.string" => "The Problem field must be a string",
+
+                "action.required" => "The action field is required",
+                "action.string" => "The action field must be a string",
+
+                "severity.required" => "The severity field is required",
+                "severity.numeric" => "The severity field must be numeric",
+                "severity.between" => "The severity field must be from 0 to 10",
+
+                "solution.string" => "The solution field must be a string",
+
+                "solution_ai.string" => "The solution_ai field must be a string",
             ]);
             return response()->json([
                 'status' => "success"
