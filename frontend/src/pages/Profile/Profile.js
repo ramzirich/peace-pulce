27312af78a4c34bf2b5bd980236a0ccb96bb5b6f
ramziriche <ramziriche96@gmail.com>
2024-01-20 21:00:16 +0200

    frontend: upload picture

--- frontend/src/pages/Profile/Profile.js
+++ frontend/src/pages/Profile/Profile.js
@@ -1,19 +1,53 @@
 import axios from "axios"
-import React, { useState } from "react"
-import { Alert, Button, Image, StyleSheet, View } from "react-native"
+import React, { useEffect, useState } from "react"
+import { Alert, Button, Image, StyleSheet, TouchableOpacity, View } from "react-native"
 // import LinearGradient from "react-native-linear-gradient"
 import { config } from "../../../config"
 import { useSelector } from "react-redux"
-// import {launchCamera,launchImageLibrary} from 'react-native-image-picker';
+import {launchCamera,launchImageLibrary} from 'react-native-image-picker';
 import AsyncStorage from "@react-native-async-storage/async-storage"
 import LinearGradient from "react-native-linear-gradient"
+import { Input } from "../../reusable/elements/Input/Input"
+import { ProfileInput } from "../../reusable/elements/Input/ProfileInput"
+import FooterButtons from "../../reusable/components/footerButtons/footerButtons"
 
-export default Profile = () =>{
+export default Profile = ({navigation}) =>{
     const [image, setImage] = useState(null)
     const [imageuri, setImageuri] = useState(null)
-    const {userInfo} = useSelector(state => state.userInfoReducer)
+    const {userInfo} = useSelector(state => state.userInfoReducer);
 
-    const pickImage = () => {
+    useEffect(() =>{
+        const uploadImage = async () => {
+            try {
+            const formData = new FormData();
+            formData.append('image', {
+                uri: image.assets[0].uri,
+                type: image.assets[0].type,
+                name: image.assets[0].fileName,
+            });
+            formData.append('image', image.assets[0]);
+            
+            const authToken = await AsyncStorage.getItem('authToken');
+            const response = await axios.post(`${config.apiUrl}/image`,formData, {
+                headers: {
+                    'Authorization': `Bearer ${authToken}`,
+                    'Content-Type': 'multipart/form-data',
+                },
+            });
+            
+                if (response.status === 200) {
+                    console.log('Image uploaded successfully');
+                    } else {
+                    console.error('Error uploading image. Server responded with:', response);
+                    }
+            } catch (error) {
+                console.error('Error uploading image:', error);
+            }
+        };
+        uploadImage();
+    },[image])
+
+    const pickImage = async() => {
         let options = {
             storageOptions:{
                 path:'image'
@@ -22,33 +56,8 @@ export default Profile = () =>{
         launchImageLibrary(options, response=>{
             setImage(response)
             setImageuri(response.assets[0].uri)
-            console.log(response)
         })
     };
-
-    const uploadImage = async () => {
-        const formData = new FormData();
-        // formData.append('image', {
-        //     uri: image.assets[0].uri,  // Use uri instead of image.fileName
-        //     type: image.assets[0].type,
-        //     name: image.assets[0].fileName,
-        // });
-        formData.append('image', image.assets[0]);
-        try {
-            const authToken = await AsyncStorage.getItem('authToken');
-            const response = await axios.post(`${config.apiUrl}/image`, {image: formData}, {
-                headers: {
-                    'Authorization': `Bearer ${authToken}`,
-                  'Content-Type': 'multipart/form-data',
-                },
-            });
-      
-            const result = await response.json();
-            console.log('Image upload result:', result);
-          } catch (error) {
-            console.error('Error uploading image:', error);
-          }
-        };
     return(
     //     // <LinearGradient 
     //     //     colors={['#8962f3', '#4752e2','#214ae2']} 
@@ -64,8 +73,67 @@ export default Profile = () =>{
 
     //     {/* </LinearGradient> */}
     // )
-        <LinearGradient colors={['#8962f3', '#4752e2','#214ae2']}  style={styles.bigContainer}>
+        <LinearGradient colors={[ '#214ae2', '#4752e2','#8962f3']}  style={styles.bigContainer}>
+            <View style={styles.medium_container}>
+                <View>
+                    <TouchableOpacity onPress={pickImage}>
+                        {imageuri ? <Image source={{uri:imageuri}} style={styles.img} />
+                            :
+                            <Image  style={styles.img} 
+                                source={{uri: `${config.imgUrl}${userInfo.img_url}`}}/>
+                        }
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.small_container}>
+                    <View style={styles.spacebtw}>
+                        <View style={styles.width_fourty_five}>  
+                            <ProfileInput
+                                //   onChangeText={text => handleOnchange(text, 'first_name')}
+                                //   onFocus={() => handleError(null, 'first_name')}
+                                label="First name"
+                                placeholder= {userInfo.first_name}
+                                //   error={errors.first_name} 
+                            />
+                        </View>
+                        
+                        <View style={styles.width_fourty_five}>  
+                            <ProfileInput
+                                //   onChangeText={text => handleOnchange(text, 'first_name')}
+                                //   onFocus={() => handleError(null, 'first_name')}
+                                label="Last name"
+                                placeholder= {userInfo.last_name}
+                                //   error={errors.first_name} 
+                            />
+                        </View>
+                    </View>
+                </View>
 
+                <View style={styles.gap_col}>
+                    <ProfileInput
+                        //   onChangeText={text => handleOnchange(text, 'first_name')}
+                        //   onFocus={() => handleError(null, 'first_name')}
+                        label="Email"
+                        placeholder= {userInfo.email}
+                        //   error={errors.first_name} 
+                    />
+                    <ProfileInput
+                        //   onChangeText={text => handleOnchange(text, 'first_name')}
+                        //   onFocus={() => handleError(null, 'first_name')}
+                        label="Password"
+                        placeholder= {userInfo.last_name}
+                        password
+                        //   error={errors.first_name} 
+                    />
+                    <ProfileInput
+                        //   onChangeText={text => handleOnchange(text, 'first_name')}
+                        //   onFocus={() => handleError(null, 'first_name')}
+                        label="Phone Number"
+                        placeholder= {userInfo.phone}
+                        //   error={errors.first_name} 
+                    />
+            </View>
+            </View>
+            <FooterButtons navigation={navigation} />
         </LinearGradient>
    )
 }
@@ -73,6 +141,30 @@ export default Profile = () =>{
 const styles = StyleSheet.create({
     bigContainer:{
         flex:1,
-        paddingTop:40
+        paddingTop:40,
+        
+    },
+    medium_container:{
+        paddingHorizontal:20
+    },
+    small_container:{
+        paddingTop:30
+    },
+    img:{
+        height:120,
+        width:120,
+        borderRadius:60,
+        resizeMode:'stretch'
+    },
+    spacebtw:{
+        flexDirection:'row',
+        justifyContent:'space-between'
+    },
+    width_fourty_five:{
+        width:"45%"
+    },
+    gap_col:{
+        flexDirection:'column',
+        gap:10
     }
 })
---
+++ frontend/src/reusable/elements/Input/ProfileInput.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import {View, Text, TextInput, StyleSheet} from 'react-native';
+import { CustomColors } from '../../../styles/color';
+
+export const ProfileInput = ({
+  label,
+  error,
+  password,
+  onFocus = () => {},
+  ...props
+}) => {
+  const [hidePassword, setHidePassword] = React.useState(password);
+  const [isFocused, setIsFocused] = React.useState(false);
+  return (
+    <View style={{marginBottom: 10}}>
+      <Text style={style.label}>{label}</Text>
+      <View
+        style={[
+          style.inputContainer,
+          {
+            borderColor: error
+              ? CustomColors.red
+              : isFocused
+              ? '#e782f5'
+              : "#e782f5",
+            alignItems: 'center',
+            borderWidth:1
+          },
+        ]}>
+        {/* <Icon
+          name={iconName}
+          style={{color: COLORS.darkBlue, fontSize: 22, marginRight: 10}}
+        /> */}
+        <TextInput
+          autoCorrect={false}
+          onFocus={() => {
+            onFocus();
+            setIsFocused(true);
+          }}
+          onBlur={() => setIsFocused(false)}
+          secureTextEntry={hidePassword}
+          placeholderTextColor={CustomColors.white}
+          style={{color: CustomColors.white, flex: 1, borderRadius:5}}
+          {...props}
+        />
+        {/* {password && (
+          <Icon
+            onPress={() => setHidePassword(!hidePassword)}
+            name={hidePassword ? 'eye-outline' : 'eye-off-outline'}
+            style={{color: CustomColors.darkBlue, fontSize: 22}}
+          />
+        )} */}
+      </View>
+      {error && (
+        <Text style={{marginTop: 7, color: CustomColors.red, fontSize: 12}}>
+          {error}
+        </Text>
+      )}
+    </View>
+  );
+};
+
+const style = StyleSheet.create({
+  label: {
+    marginVertical: 4,
+    fontSize: 14,
+    color: CustomColors.white,
+  },
+  inputContainer: {
+    height: 45,
+    backgroundColor: "transparent",
+    flexDirection: 'row',
+    paddingHorizontal: 15,
+    borderWidth: 0.5,
+    borderRadius:5,
+    // borderWidth:1,
+    // borderBlockColor:'#e782f5',
+    // borderColor:'#e782f5'
+  },
+});
