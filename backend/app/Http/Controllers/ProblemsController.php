2a336e548f934b6b14df9f56225f218fb3555b59
ramziriche <ramziriche96@gmail.com>
2024-01-21 00:59:24 +0200

    backend: add problem folder

--- backend/app/Http/Controllers/ProblemsController.php
+++ backend/app/Http/Controllers/ProblemsController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Manager\UserSpecificGenericManager;
+use App\Models\Problem;
 use Illuminate\Http\Request;
 
 class ProblemsController extends Controller
 {
     //
+    private $_problem, $_userSpecificGenericManager;
+    public function __construct(){
+        $_problem = new Problem();
+        $_userSpecificGenericManager = new UserSpecificGenericManager($_problem);
+    }
+
+    public function createProblem(Request $request){
+
+    }
 }
