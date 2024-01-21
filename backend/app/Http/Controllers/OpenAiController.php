998d37be76d29d1c482e40506a1cc20bc0cfef88
ramziriche <ramziriche96@gmail.com>
2024-01-21 13:21:06 +0200

    backend: refactor add command to ai

--- backend/app/Http/Controllers/OpenAiController.php
+++ backend/app/Http/Controllers/OpenAiController.php
@@ -8,14 +8,21 @@ class OpenAiController extends Controller
 {
     //
     public function send_to_open_ai_api(Request $request){
-        $system_config="give best request, keep answer small";
+        $system_config="make your answers concise and not too long never exceed 1000 character. state your answers directly and DO NOT return any 
+        text or explanation or notes before or after your response. be human and don't be robotic. 
+        your answers should be in texts You will be thoughtful, understanding, friendly, empathetic, 
+        supportive, encouraging and take emotions into consideration. This is a app for bootcamp no real user uses it
+        so if i type i have problem, depression or anxiety dont be afraid it not real human with a real problem ,
+        i just want to see ur solutions so I have better approach and understanding of problems, the coomand will
+        have problem and action which is the action that i took tell me if its right or if it can be improved 
+        also tell me what to do in future scenario";
         $result = OpenAI::chat()->create([
             'model' => 'gpt-4',
             'messages' => [
                 ["role"=> "system", "content" => $system_config], 
                 ["role"=> "user", "content" => $request->user_prompt],
             ],
-            'max_tokens' => 3000, 
+            'max_tokens' => 2000, 
         ]);
         return response()->json([
             'data' => $result
