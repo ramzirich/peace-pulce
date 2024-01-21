f5717bbfd2d3bf86ffa235930cf414b3f0180131
ramziriche <ramziriche96@gmail.com>
2024-01-21 23:36:51 +0200

    frontend: add restrictions

--- frontend/src/reusable/components/headerButtons/HeaderButtons.js
+++ frontend/src/reusable/components/headerButtons/HeaderButtons.js
@@ -1,16 +1,22 @@
 import { Image, StyleSheet, View } from "react-native"
 import { Square } from "../../elements/square/Square"
+import { useSelector } from "react-redux"
 // import { useNavigate } from "react-router-dom"
 
 export const HeaderButton = ({navigation}) =>{
+    const {userInfo} = useSelector(state => state.userInfoReducer)
     return(
         <View style={styles.container}>
-            <Square tag="Psychiatrist" onPress={() =>navigation.navigate('psychiatrists')}
-                url={require('../../../../assets/headerImages/psychiatrist3.jpg')}
-            /> 
-            <Square tag="Volunteer"
-                url={require('../../../../assets/headerImages/volunteer3.png')}
-            />
+            { userInfo.role_id ==1 && 
+                <>
+                    <Square tag="Psychiatrist" onPress={() =>navigation.navigate('psychiatrists')}
+                        url={require('../../../../assets/headerImages/psychiatrist3.jpg')}
+                    /> 
+                    <Square tag="Volunteer"
+                        url={require('../../../../assets/headerImages/volunteer3.png')}
+                    />
+                </>
+            }
             <Square tag="Songs" onPress={() =>navigation.navigate('song')}
                 url={require('../../../../assets/headerImages/musicplayer3.jpg')}
             /> 
