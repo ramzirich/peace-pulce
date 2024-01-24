e926323692c3cfd924416273ab6289246e3b02da
ramziriche <ramziriche96@gmail.com>
2024-01-24 23:49:58 +0200

    frontend: style vounteer info

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -32,7 +32,7 @@ function App(): React.JSX.Element {
   return (
     <Provider store={storee}>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="login"> 
+        <Stack.Navigator initialRouteName="home"> 
           <Stack.Screen name="login" component={Login} options={{ headerShown: false }} /> 
           <Stack.Screen name="register" component={Registration} options={{ headerShown: false }} /> 
           <Stack.Screen name="home" component={Home} options={{ headerShown: false }} /> 
--- frontend/src/pages/Volunteer/VolunteerUser.js
+++ frontend/src/pages/Volunteer/VolunteerUser.js
@@ -2,13 +2,17 @@ import React from "react"
 import { config } from "../../../config"
 import LinearGradient from "react-native-linear-gradient"
 import { useSelector } from "react-redux";
-import { Image, ScrollView, Text, View } from "react-native";
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
+import { CustomColors } from "../../styles/color";
+import { useNavigation } from "@react-navigation/native"
 
 export default VolunteerUser = ({route}) =>{
     const {id, volunteerInfo} = route.params;
     const {userInfo} = useSelector(state => state.userInfoReducer)
-    const {first_name, last_name, about, img_url } = volunteerInfo;
+    const {first_name, last_name, about, img_url, phone } = volunteerInfo;
     const imgUrl = `${config.imgUrl}${img_url}` 
+
+    const navigation = useNavigation()
     return(
         <LinearGradient style={styles.big_container}
         colors={['#8962f3', '#4752e2','#214ae2']} 
@@ -43,3 +47,92 @@ export default VolunteerUser = ({route}) =>{
         </LinearGradient>
     )
 }
+
+const styles = StyleSheet.create({
+    big_container:{
+        flex:1,
+        paddingTop: 40,
+        paddingHorizontal:20,     
+    },
+    image:{
+        height: 300,
+        width:'100%',
+        resizeMode:'stretch'
+    },
+    profile:{
+        flexDirection:'row',
+        marginBottom:20,
+        justifyContent:'space-between',
+    },
+    white:{
+        color:CustomColors.white,
+    },
+    fullname:{
+        flexDirection:'column',
+        width:'55%', 
+        justifyContent:'center'
+    },
+    name:{
+        fontSize:26,
+        fontWeight:'bold',
+        color: CustomColors.black,
+        letterSpacing:1,
+    },
+    imgUrl:{
+        height:120,
+        width:120,
+        borderRadius:60,
+    },
+    aboutContainer:{
+        paddingTop:15
+    },
+    aboutheader:{
+        paddingBottom:5,
+        color: CustomColors.white,
+        fontSize:22,
+        fontWeight:'500'
+    },
+    about:{
+        color: CustomColors.black,
+        fontWeight:'500',
+        letterSpacing:0.2,
+        color:CustomColors.white,
+    },
+    cost_circle:{
+        height:45,
+        width:45,   
+        backgroundColor:CustomColors.blue,
+        borderRadius:22.5,
+    },
+    subTitle:{
+        fontSize:14,
+        fontWeight:'bold'
+    },
+    request:{
+        color: CustomColors.white,
+        alignSelf:'flex-end',
+        paddingBottom:10
+    },
+    comment:{
+        color: CustomColors.white,
+        alignSelf:'flex-start',
+        paddingBottom:10
+    },
+    spacebtw:{
+        flexDirection:'row', 
+        justifyContent:'space-between',
+        alignItems:'center',
+        alignItems:'center'
+    },
+    row_five:{
+        flexDirection:'row',
+        gap:5,
+        alignItems:'center'
+    },
+    icon:{
+        backgroundColor:'transparent', 
+        width:16, 
+        height:16, 
+        tintColor:'white'
+    },
+})
