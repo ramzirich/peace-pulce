553ef7130b3a4e516240d1421ad45331fc603705
ramziriche <ramziriche96@gmail.com>
2024-01-24 23:37:45 +0200

    frontend: add about

--- frontend/src/pages/Volunteer/VolunteerUser.js
+++ frontend/src/pages/Volunteer/VolunteerUser.js
@@ -13,42 +13,33 @@ export default VolunteerUser = ({route}) =>{
         <LinearGradient style={styles.big_container}
         colors={['#8962f3', '#4752e2','#214ae2']} 
         >     
-        <ScrollView showsVerticalScrollIndicator={false}>
-        {/* <Image source={{uri : imgUrl}} style={styles.image} /> */}
-        <View style={styles.profile}>
-            <View style={styles.fullname}>
-                <Text style={[styles.name, styles.white]}>Dr. {first_name}</Text>
-                <Text style={[styles.name, styles.white]}>{last_name}</Text>
-                <Text style={[styles.degree, styles.white]}>{specialization}</Text>
-                <Text style={[styles.degree, styles.white]}>{degree}</Text>
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View style={styles.profile}>
+                    <View style={styles.fullname}>
+                        <Text style={[styles.name, styles.white]}>{first_name}</Text>
+                        <Text style={[styles.name, styles.white]}>{last_name}</Text>
+                        <View style={{marginTop: 7, flexDirection:'row', alignItems:'center', gap:8}}>
+                            <View>
+                                <Image source={require('../../../assets/images/phone.png')} 
+                                        style={[styles.icon]}/>
+                            </View>
+                            <View>
+                                <Text onPress={() =>navigation.navigate('call', {phone})} style={{color:'white'}}>
+                                    Call me
+                                </Text>
+                            </View>
+                        </View>
+                    </View>
+                    <View>
+                        <Image source={{uri : imgUrl}} style={styles.imgUrl} />
+                    </View>
+                </View>
                 
-                <View style={{flexDirection:"row", gap:30, alignItems:'center'}}>
-                    <View>
-                        <Text style={[{fontSize:12, fontWeight:300}, styles.white]}>${hourly_rate}/hr</Text>
-                    </View>    
+                <View style={styles.aboutContainer}>
+                    <Text style={styles.aboutheader}>About</Text>
+                    <Text style={styles.about}>{about}</Text>
                 </View>
-                <View style={{marginTop: 7, flexDirection:'row', alignItems:'center', gap:8}}>
-                    <View>
-                        <Image source={require('../../../assets/images/phone.png')} 
-                                style={[styles.icon]}/>
-                    </View>
-                    <View>
-                        <Text onPress={() =>navigation.navigate('call', {phone})} style={{color:'white'}}>
-                            Call me
-                        </Text>
-                    </View>
-                </View>
-            </View>
-            <View>
-                <Image source={{uri : imgUrl}} style={styles.imgUrl} />
-            </View>
-        </View>
-        
-        <View style={styles.aboutContainer}>
-            <Text style={styles.aboutheader}>About</Text>
-            <Text style={styles.about}>{about}</Text>
-        </View>
-        </ScrollView>
+            </ScrollView>
         </LinearGradient>
     )
 }
