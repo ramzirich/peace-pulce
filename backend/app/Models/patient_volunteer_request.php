d40cda6ff0a56ba778a699f1c1210ee619c61763
ramziriche <ramziriche96@gmail.com>
2024-01-25 01:01:03 +0200

    backend: add patient request volunteer model

---
+++ backend/app/Models/patient_volunteer_request.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class patient_volunteer_request extends Model
+{
+    use HasFactory;
+}
