4addf9aa65d1d2b7c4724b4961b44ab24dd37e35
ramziriche <ramziriche96@gmail.com>
2024-01-25 14:58:16 +0200

    backend:refactor note

--- backend/app/Http/Controllers/DoctorNoteController.php
+++ backend/app/Http/Controllers/DoctorNoteController.php
@@ -28,7 +28,7 @@ class DoctorNoteController extends Controller
         $userDoctor= new  Doctors();
         $doctorId = $userDoctor->where('user_id',  $this->user->id)->first()->id;
         $model = $this->doctor_note->where('doctor_id',  $doctorId)->where('patient_id', $id)
-            ->with(['user', 'doctor'])->get()->first();
+            ->with(['user', 'patient'])->get()->first();
         return $model; 
     } 
     public function createDoctorNote(Request $request){
@@ -50,14 +50,25 @@ class DoctorNoteController extends Controller
                 return ExceptionMessages::Error('This user is not a patient', 400);
             }
 
-            $genericManagerRequest = new UserSpecificGenericManager(new Patient_doctor_request());
-            $patientDoctorRequestModel = $genericManagerRequest->getByColumn('patient_id', 
-                $data['patient_id'], 'doctor_id');
-            if($patientDoctorRequestModel ==[] || $patientDoctorRequestModel->request != 'accepted'){
-                return ExceptionMessages::Error('This user is not your patient', 400);
-            } 
+            // $genericManagerRequest = new UserSpecificGenericManager(new Patient_doctor_request());
+            // $patientDoctorRequestModel = $genericManagerRequest->getByColumn('patient_id', 
+            //     $data['patient_id'], 'doctor_id');
+            // if($patientDoctorRequestModel ==[] || $patientDoctorRequestModel->request != 'accepted'){
+            //     return ExceptionMessages::Error('This user is not your patient', 400);
+            // } 
             
-            return $this->userSpecificGenericManager->createWithSpecificUser($request);
+            $userDoctor= new  Doctors();
+            $doctorId = $userDoctor->where('user_id',  $this->user->id)->first()->id;
+
+            $this->doctor_note->patient_id = $request['patient_id'];
+            $this->doctor_note->doctor_id = $doctorId;
+            $this->doctor_note->note = $request['note'];
+            $this->doctor_note->save();
+            
+            return response()->json([
+                'status'=> 'success',
+                'data'=> $this->doctor_note
+            ], 201);
         }catch(\Exception $exception){
             return ExceptionMessages::Error($exception->getMessage());
         }
@@ -75,7 +86,14 @@ class DoctorNoteController extends Controller
                 $data = $request->except(['patient_id']);
                 $request->replace($data);
             }
-            return $this->userSpecificGenericManager->updateForSpecificUser($request, $id, "doctor_id");
+            
+            $model = $this->doctor_note->find($id);
+            $model->note = $request['note'];
+            $model->save();
+            return response()->json([
+                'status'=> 'success',
+                'data'=> $model
+            ], 200);
         }catch(\Exception $exception){
             return ExceptionMessages::Error($exception->getMessage());
         }
