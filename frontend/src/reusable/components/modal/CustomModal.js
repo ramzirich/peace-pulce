fd8a87a2e8e94ecce8ba5adfdfe2f56f43619e7f
ramziriche <ramziriche96@gmail.com>
2024-01-26 12:48:35 +0200

    frontend:add more to container styles

--- frontend/src/reusable/components/modal/CustomModal.js
+++ frontend/src/reusable/components/modal/CustomModal.js
@@ -1,9 +1,9 @@
-import { StyleSheet, View } from "react-native"
+import { StyleSheet, Text, View } from "react-native"
 
 export default CustomModal = () =>{
     return(
         <View style={styles.container}>
-
+            <Text></Text>
         </View>
     )
 }
@@ -11,6 +11,9 @@ const styles = StyleSheet.create({
     container:{
         padding:30,
         height:200,
-        width:'90%'
+        width:'90%',
+        flexDirection:'row',
+        alignItems:'center',
+        justifyContent:'center'
     }
 })
