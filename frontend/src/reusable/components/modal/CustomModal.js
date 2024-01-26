81b3708edfbcc21815b4da9ad5dc8c28d87f4235
ramziriche <ramziriche96@gmail.com>
2024-01-26 12:49:24 +0200

    frontend:add text to modal

--- frontend/src/reusable/components/modal/CustomModal.js
+++ frontend/src/reusable/components/modal/CustomModal.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from "react-native"
 export default CustomModal = () =>{
     return(
         <View style={styles.container}>
-            <Text></Text>
+            <Text>Changes Saved</Text>
         </View>
     )
 }
